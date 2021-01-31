ed7b6888678b6d04912cec9874600e9083083ef1
Akuukis <akuukis@kalvis.lv>
2021-01-31 18:30:22 +0200

    chore: publish 0.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='beancount_allocate',
-    version='0.1.3',
+    version='0.1.4',
     description='Plugin for Beancount to collaborate within one ledger.',
 
     long_description=long_description,
